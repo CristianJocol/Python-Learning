ec8291787efc706c226d9e81e124f21616c83442
Cristian <cjocolc@miumg.edu.gt>
2022-09-13 22:05:30 -0600

    bases

---
+++ Proyectos/madLibs.py
@@ -0,0 +1,24 @@
+# Objetivo: Concatenar cadenas de caracteres
+# Aprende a programa con ____________.
+
+#Como yo lo realizaría: 
+
+# print('ingresa el nombre de tu organización: ')
+# organizacion = input()
+
+# print('Aprende a programar con ' + organizacion + '.')
+
+#Otras alternativas: 
+
+# print('ingresa el nombre de tu organización: ')
+# organizacion = input()
+# print('Aprende a programar con {}'.format(organizacion))
+
+#Separador --> Otra alternativa f-string form
+
+# print('ingresa el nombre de tu organización: ')
+# organizacion = input()
+# print(f"Aprende a programar con {organizacion}")
+
+#Juego palabras locas --> "Ahorcado" 
+
